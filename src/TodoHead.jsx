e1617bd0bbfa82e0344092d506560615c32c76b3
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-12 23:01:16 +0530

    seventh commit

--- src/TodoHead.jsx
+++ src/TodoHead.jsx
@@ -3,7 +3,7 @@ import { GrCompliance } from "react-icons/gr";
 function TodoHead() {
   return (
     <>
-      <div className="d-flex flex-row align-items-center justify-content-center mt-3  mx-5 border-danger border-bottom border-top border-2">
+      <div className="d-flex flex-row align-items-center col-lg-10 justify-content-center mt-3  mx-5 border-danger border-bottom border-top border-2">
         <h1 className="text-center">Todo App</h1>
         <GrCompliance 
         color="brown"
