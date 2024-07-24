da1b96888f7a56882f3143f586255d4a7829f1d2
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-25 03:59:36 +0530

    Todo commit

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,86 +1,77 @@
 import { useContext, useState } from "react";
 import { IoAdd } from "react-icons/io5";
 import { todoItemContext } from "./store/todo-item-store";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+
+
 
 function AddTodo() {
   const { addNewItem, todoItems } = useContext(todoItemContext);
   const [task, setTask] = useState("");
-  const [date, setDate] = useState(null);
-  const [id, setId] = useState(1);
+  const [date, setDate] = useState("");
+  const [id, setid] = useState(1)
 
   const onNewTask = (e) => {
     setTask(e.target.value);
-  };
+  }
 
-  const onNewDate = (date) => {
-    setDate(date);
-  };
+  const onNewDate = (e) => {
+    setDate(e.target.value);
+  }
 
   const handleAddButtonClick = (event) => {
+    console.log(id)
     event.preventDefault();
 
-    if (todoItems.some(n => n.name === task) && todoItems.some(n => n.date === date?.toString())) {
+    if (todoItems.some(n => n.name === task) && todoItems.some(n => n.date === date)) {
       alert("This task already exists.");
       return;
     }
 
-    if (!task && !date) {
-      alert("Please add a task and date");
-    } else if (!task) {
-      alert("Please add a task");
-    } else if (!date) {
-      alert("Please add a date");
+    if (task === "" && date === "") {
+      alert('Please add a task and date');
+    } else if (task === "") {
+      alert('Please add a task');
+    } else if (date === "") {
+      alert('Please add a date');
     } else {
-      addNewItem(task, date?.toString(), id);
+      addNewItem(task, date, id);
       setTask("");
-      setDate(null);
-      setId(id + 1);
+      setDate("");
+      setid(()=>id+1)
     }
-  };
+  }
 
   return (
     <>
-      <div className="d-flex flex col-lg-10">
-        <h2 className="col-lg-4 col-4">Task</h2>
-        <h2 className="col-lg-4 col-4">Date</h2>
-        <div className="col-lg-4 col-4"></div>
-      </div>
+    <div className="d-flex flex col-lg-10">
+    <h2 className=" col-lg-4 col-4">Task</h2> 
+      <h2 className=" col-lg-4 col-4">Date</h2>
+      <div className="col-lg-4 col-4"></div>
+    </div>
       
-      <form 
-        className="d-flex flex-row col-lg-10 justify-content-around rounded border-bottom border-4 border-warning shadow py-2 mx-3" 
-        onSubmit={handleAddButtonClick} 
-        style={{ backgroundColor: 'white' }}
-      >
-        <div className="col-lg-4 col-5">
-          <input 
-            type="text" 
-            value={task} 
-            onChange={onNewTask}
-            className="col-lg-6 col-10 border border-2 rounded border-dark-subtle"
-            style={{ height: '33px', outline: 'none', border: 'none' }}
-          />
-        </div>
-        <div className="col-lg-4 col-5">
-          <DatePicker 
-            selected={date} 
-            onChange={onNewDate} 
-            className="col-lg-12 col-10 rounded border border-2 border-dark-subtle" 
-            dateFormat="dd/MM/yyyy"
-            placeholderText="Select a date"
-          />
-        </div>
-        <div className="col-lg-4 col-2">
-          <button 
-            type="submit" 
-            className="col-lg-4 btn btn-success" 
-            style={{ height: '33px' }}
-          >
-            <IoAdd size={20} className="mb-5" />
-          </button>
-        </div>
-      </form>
+    <form className="d-flex flex-row col-lg-10 justify-content-around rounded border-bottom border-4 border-warning   justify-content-around  shadow py-2 mx-3" onSubmit={handleAddButtonClick} style={{backgroundColor: 'white'}}>
+      <div className="col-lg-4 col-5">
+        <input type="text" 
+               value={task} 
+               onChange={onNewTask}
+               className="col-lg-6 col-10  border border-2 rounded border-dark-subtle"
+               style={{height :'33px', outline: 'none', border:'none'}}>
+        </input>
+      </div>
+      <div className="col-lg-4  col-5">
+        <input type="date" 
+               value={date}
+               onChange={onNewDate}
+               className="col-lg-6 col-10  rounded border border-2 border-dark-subtle "
+               style={{minHeight:'30px', maxHeight :'30px', outline: 'none'}}>
+        </input>
+      </div>
+      <div className="col-lg-4 col-2">
+        <button type="submit" 
+                className="col-lg-4  btn btn-success" style={{height: '33px'}}><IoAdd size={20} className="mb-5" />
+        </button>
+      </div>
+    </form>
     </>
   );
 }
